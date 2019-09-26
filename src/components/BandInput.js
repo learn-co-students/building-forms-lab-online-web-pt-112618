3700ca69e72674e15269641c921af3cfaf49c75b
Chris Philbin <chris@chrisphilbin.net>
2019-09-26 07:39:16 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,38 @@
-// Add BandInput component
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class BandInput extends Component {
-  render() {
-    return(
-      <div>
-        Band Input
-      </div>
-    )
-  }
-}
 
-export default BandInput
+	state = {
+		name: 
+	}
+
+	handleChange = (event) => {
+		this.setState = ({
+			name: event.target.text
+		})
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault()
+		this.props.addBand(this.state)
+	}
+
+	render() {
+		return(
+			<div>
+				<form>
+					<input type="text" onChange={this.handleChange} value={this.state.name}>
+					<input type="submit" onSubmit={ (event) => this.handleSubmit(event)}>
+				</form>
+			</div>
+			)
+	}
+
+}
+const mapDispatchToProps = dispatch => {
+	return {
+		addBand: formData => dispatch({ type: 'ADD_BAND', payload: formData })
+	}
+}
+export default connect(null, mapDispatchToProps)(CreateTodo)
